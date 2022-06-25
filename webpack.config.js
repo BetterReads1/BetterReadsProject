370f415dd91116da51828748cf446980d5417275
Carol Xia <c.xia.98@gmail.com>
2022-06-24 21:27:58 -0700

    created boilerplate code

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          },
+        }
+      },
+      {
+        test:/\.jsx?$/,
+        exclude:/node-module/,
+        use: {
+            loader:'babel-loader',
+            options:{
+                presets:['@babel/preset-env', '@babel/preset-react']
+            }
+        }
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+       ],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+     template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+  devtool: 'inline-source-map',
+  
+};
