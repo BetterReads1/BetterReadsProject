a82a546093f6da4e2dce3a4e07d79af3da9e5de7
Ryan Huie <rhpurple1023@gmail.com>
2022-06-25 14:38:33 -0700

    Testing server reconfigured npm start

--- Server/server.js
+++ Server/server.js
@@ -3,12 +3,28 @@ const app = express();
 const port = 3000;
 
 
-app.post('/', (req, res) => {
-    res.status(200).json("WOO, IT ConnecteD!")
+app.get('/',  (req, res) => {
+    res.status(200).send("WOO, IT ConnecteD!")
 })
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
+
+
+app.use((req, res) => {
+    res.status(404).json("Page not found");
+})
+
+app.use((err, req, res, next) => {
+    const defaultError = {
+        log: "Error in unknown middleware",
+        status: 400,
+        message: {err: "Error sent in response"}
+    }
+    const errorObj = Object.assign(defaultError, err);
+    console.log(errObj.log);
+    return res.staus(errorObj.status).json(errorObj.message)
+})
 
 module.exports = app;
