75b2fca391363af3ffd9fb2aa2b9f15f719d3b10
Carol Xia <c.xia.98@gmail.com>
2022-06-27 10:47:48 -0700

    added input form to createPost

--- client/CreatePost.js
+++ client/CreatePost.js
@@ -2,25 +2,90 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 const axios = require('axios');
 
-class CreatePost extends Component {
-    // constructor(props) {
-    //   // super(props);
-    // }
+const CreatePost = () => {
 
-    componentDidMount() {
-      axios.get('/')
-        .then( res => {
-          console.log(res);
-        })
-        .catch(err => {
-          console.log(err);
-        })
-    }
-
-    render() {
-      return (
-        <div> CREATE POST </div>
-      );
-    }
-  }
+  return (
+    <div>
+      <form>
+        Name:<input 
+          id="name"
+          name="name"
+          type="text"
+        //   onChange={event => setFirstName(event.target.value)}
+        //   value={firstName}
+        /> <br/>
+        Title:<input 
+          id="title"
+          name="title"
+          type="text"
+        //   onChange={event => setFirstName(event.target.value)}
+        //   value={firstName}
+        /><br/>
+        Author: <input 
+          id="author"
+          name="author"
+          type="text"
+        //   onChange={event => setFirstName(event.target.value)}
+        //   value={firstName}
+        /><br/>
+        Caption: <input 
+          id="caption"
+          name="caption"
+          type="text"
+        //   onChange={event => setFirstName(event.target.value)}
+        //   value={firstName}
+        /><br/>
+       Plotline: <input 
+          id="plotline"
+          name="plotline"
+          type="text"
+        //   onChange={event => setFirstName(event.target.value)}
+        //   value={firstName}
+        /><br/>
+        Unpredicability: <input 
+          id="unpredictability"
+          name="unpredictability"
+          type="text"
+        //   onChange={event => setFirstName(event.target.value)}
+        //   value={firstName}
+        /> <br/>
+        Pace: <input 
+          id="pace"
+          name="pace"
+          type="text"
+        //   onChange={event => setFirstName(event.target.value)}
+        //   value={firstName}
+        /> <br/>
+        Writing Style: <input 
+          id="writing-style"
+          name="writing-style"
+          type="text"
+        //   onChange={event => setFirstName(event.target.value)}
+        //   value={firstName}
+        /> <br/>
+        Ending: <input 
+          id="ending"
+          name="ending"
+          type="text"
+        //   onChange={event => setFirstName(event.target.value)}
+        //   value={firstName}
+        /> <br/>
+        Overall Enjoyability: <input 
+          id="overall_enjoyability"
+          name="overall_enjoyability"
+          type="text"
+        //   onChange={event => setFirstName(event.target.value)}
+        //   value={firstName}
+        /> <br/>
+        Tags:<input 
+          id="tags"
+          name="tag"
+          type="text"
+        //   onChange={event => setFirstName(event.target.value)}
+        //   value={firstName}
+        />
+      </form>
+    </div>
+  )
+}
   export default CreatePost;
