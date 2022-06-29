e57a2eca74951ca2ed14ba71ca4babaf50c0394c
RadiantGH <iangrapeo@gmail.com>
2022-06-29 10:25:59 -0700

    Moved sample code to separate folder.

--- Server/Models/sqldb.js
+++ Server/Models/sqldb.js
@@ -1,13 +1,20 @@
-//Jim's database
-const db = require('knex')({
-    client: 'mysql2',
-    connection: {
-      host: '23.235.206.125',
-      port: 3306,
-      user: 'jimtermini_bradmin',
-      password: '123br456',
-      database: 'jimtermini_bestestreads',
-    },
-  });
+/*
+* ==================================================
+*   This block added by Jim White as sample for
+*   database integration / setup.
+* ==================================================
+*/
 
-  export default db;
+const db = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host: '23.235.206.125',
+    port: 3306,
+    user: 'jimtermini_bradmin',
+    password: '123br456',
+    database: 'jimtermini_bestestreads',
+  },
+});
+
+// !NOT USING
+app.set('db', db); //* Allows the whole app to use the db
