7d5df0cd081866d868f5fe763b85546325800124
RadiantGH <iangrapeo@gmail.com>
2022-06-28 19:46:12 -0700

    backend comments

--- Server/Controller/queryMiddleware.js
+++ Server/Controller/queryMiddleware.js
@@ -2,6 +2,74 @@ const db = require('../Models/betterReadsModel');
 
 const queryMiddleware = {}
 
+/*
+* ==================================================
+*   Before: None
+*   Receives 'title' and 'author' from 'req.body'
+*   Adds a book with title and author to 'book_table' using the ($1 and $2 params)
+*   The values for those are saved in the book_tableParamArray array
+!   We don't know where this information is coming from, since the route starts at default! ('/')
+*   Next: getBook_Id
+
+    Important Info:
+        hash refers to the tags (as in, hash tag haha)
+        
+
+*   req.body info masterlist: {
+*       title
+*       author
+*       name
+*       comments
+*       tags
+*       plotline
+*       unpredictability
+*       pace
+*       writingStyle
+*       ending
+*       overallEnjoyability
+*   }
+
+*   res.locals info masterlist: {
+*       book_id
+*       post_id
+*   }
+
+
+*   TABLES: 
+        book_table {
+            book_id,
+            title, 
+            author
+        }
+        post_table  {
+            username, 
+            comments, 
+            book_id
+            post_id,
+        }
+        
+        ! The hash table is a single layered table that ONLY CONTAINS KEYS NAMED 'HASH'
+        hash_table {
+            hash
+        }
+
+        rating_table {
+            post_id, 
+            book_id, 
+            plotline, 
+            unpredictability, 
+            pace, writing_style, 
+            ending, 
+            overall_enjoyability
+        }
+
+        post_hash_join {
+            post_id, 
+            hash_id
+        }
+
+* ==================================================
+*/
 queryMiddleware.addToBook_Table = (req, res, next) => {
     //destructure the request body
     const { title, author } = req.body;
@@ -22,6 +90,14 @@ queryMiddleware.addToBook_Table = (req, res, next) => {
     });
 }
 
+/*
+* ==================================================
+*   Before: addToBook_Table
+*   Receives the 'title' from the req body and uses it to search for it's corresponding book ID
+*   This ID is then saved in res.locals as 'book_id'
+*   Next: addToPost_Table
+* ==================================================
+*/
 queryMiddleware.getBook_Id = (req, res, next) => {
     //destructure the request body
     const { title } = req.body;
@@ -38,7 +114,14 @@ queryMiddleware.getBook_Id = (req, res, next) => {
     })
 }
 
-//adding data to post table
+/*
+* ==================================================
+*   Before: getBook_Id
+*   Receives 'name' 'comments' from "req.body" and 'book_id' from "res.locals"
+*   Inserts that information as 'username', 'comments', and 'book_id' into 'post_table'
+*   Next: getPost_Id
+* ==================================================
+*/
 queryMiddleware.addToPost_Table = (req, res, next) => {
     const { name, comments } = req.body;
     const post_tableAdd = `INSERT INTO post_table (username, comments, book_id) VALUES ($1, $2, $3) RETURNING *`
@@ -56,7 +139,15 @@ queryMiddleware.addToPost_Table = (req, res, next) => {
     });
 }
 
-//get post table id
+/*
+* ==================================================
+*   Before: 'addToPost_Table'
+*   Receives name and comments from req.body
+*   Uses that information to select the 'post_id' from 'post_table'
+*   Saves the post_id into res.locals.post_id
+*   Next: 'addToHash_Table'
+* ==================================================
+*/
 queryMiddleware.getPost_Id = (req, res, next) => {
     //destructure the request body
     const { name, comments } = req.body;
@@ -78,7 +169,20 @@ queryMiddleware.getPost_Id = (req, res, next) => {
     })
 }
 
-// adding to Hash_Table
+/*
+* ==================================================
+*   Before: 'getPost_Id'
+*   Retrieve tags from req.body
+*   These tags are a single string, the individual tags are separated by commas
+*   tagsArray are the result of splitting the tags string by its commas
+!   THE QUERY ASSUMES THAT THERE ARE THREE TAGS
+!   These three tags are then inserted into 'hash_table' all as 'hash'
+!   We have no idea what this does.
+ToDo: TEST ON SQL SERVER
+*   Somehow these hash_ids are saved into res.locals.hashOne,Two,Three respectively
+*   Next: 'addToRating_Table'
+* ==================================================
+*/
 queryMiddleware.addToHash_Table = (req, res, next) => {
     //destructure the request body
     const { tags } = req.body;
@@ -102,12 +206,22 @@ queryMiddleware.addToHash_Table = (req, res, next) => {
     //adds book title and author to database if it doesnt exist
 }
 
+
+/* 
+* ===================================================================
+*   Before: 'addToHash_Table'
+*   Gets book_id and post_id from res.locals
+*   Receives plotline, unpredictability, pace, writingStyle, ending, and overallEnjoyability from req.body
+*   INSERTS these values as post_id, book_id, plotline, unpredictability, pace, writing_style, ending, and overall_enjoyability in 'rating_table'
+*   After: 'addToPost_Hash_Join'
+* ===================================================================
+*/
 // add to rating_table
 queryMiddleware.addToRating_Table = (req, res, next) => {
     const { book_id, post_id } = res.locals;
     const {  plotline, unpredictability, pace, writingStyle, ending, overallEnjoyability } = req.body;
     const rating_tableAdd = `INSERT INTO rating_table ( post_id, book_id, plotline, unpredictability, pace, writing_style, ending, overall_enjoyability) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`
-    const rating_tableParamArray = [ post_id, book_id, plotline, unpredictability, pace, writingStyle, ending, overallEnjoyability];
+    const rating_tableParamArray = [post_id, book_id, plotline, unpredictability, pace, writingStyle, ending, overallEnjoyability];
     
     // console.log("book_id:", book_id);
     // console.log("post_id:", post_id);
@@ -126,8 +240,13 @@ queryMiddleware.addToRating_Table = (req, res, next) => {
 }
 
 
-
-// add to post_hash table
+/* Add to post_hash table
+* ==================================================
+*   Before: 'addToRating_Table'
+*   Gets hashOne, hashTwo, hashThree, and post_id
+*   Next: 'server.js'
+* ==================================================
+*/
 queryMiddleware.addToPost_Hash_Join = (req, res, next) => {
     const { hashOne, hashTwo, hashThree, post_id } = res.locals;
     //query for adding to the book table
