4fa635686d66d9c38e58937e168404cd5d79fcfd
Jim White <Jim.White@me.com>
2022-06-29 17:36:23 -0400

    Initial Post

--- client/components/BookCard.jsx
+++ client/components/BookCard.jsx
@@ -6,8 +6,8 @@ const BookCard = (props) => {
 
 
   const SingleBookCard = (
-    <div id='book-card' className='border shadow border-info'>
-      <h3>My Fancy Book Title</h3>
+    <div id='book-card' className='border shadow border-warning h-25'>
+      <p>My Fancy Book Title</p>
       by
       <h5>Me myself and Irene</h5>
     </div>
@@ -18,7 +18,7 @@ const BookCard = (props) => {
     // <SingleBookCard />
     // </div>
     <div id='book-card' className='border shadow border-info'>
-      <div className="container border border-3 rounded-3 mt-2 p-2 border-info shadow">My Fancy Book Title</div>
+      <div className="container border border-3 h-25 rounded-3 mt-2 p-4 border-info shadow">My Fancy Book Title</div>
       by
       <span className="card border border-3 rounded-3 m-20 p-25 border-warning shadow">Me myself and Irene</span>
     </div>
