c4fdd08331c68291d43547c2b970cfa5c152ef67
Ilija Bibic <ibibic@Ilijas-MBP.lan>
2022-06-30 10:54:16 -0700

    BookCard component

--- client/components/BookCard.jsx
+++ client/components/BookCard.jsx
@@ -1,27 +1,59 @@
 import React from 'react';
+import styles from '../_custom.scss'
 
 const BookCard = (props) => {
 
 
 
-
-  const SingleBookCard = (
-    <div id='book-card' className='border shadow border-info'>
-      <h3>My Fancy Book Title</h3>
-      by
-      <h5>Me myself and Irene</h5>
-    </div>
-  );
-
+  
   return (
-    // <div>
-    // <SingleBookCard />
-    // </div>
-    <div id='book-card' className='border shadow border-info'>
-      <div className="container border border-3 rounded-3 mt-2 p-2 border-info shadow">My Fancy Book Title</div>
-      by
-      <span className="card border border-3 rounded-3 m-20 p-25 border-warning shadow">Me myself and Irene</span>
-    </div>
+   
+    
+    <div id='book-card' className='container col-md-3 border shadow border-info'>
+      <img src="/./books.jpeg"></img>
+     
+      
+      <div className = "card-body card-title text-center pt-2">
+        <strong>Title by Author</strong>
+        </div>
+       
+        
+      <div className ="row">
+      <div className ="align-items-center col-md-6 text-end  border"><strong>Genre:</strong></div>
+      <div class = "best-value" className = "border best-value col-md-6 text-start ">Horror</div>
+      
+      </div>
+      <div className ="row">
+      <div className ="align-items-center col-md-6 text-end  border"><strong>Pages:</strong></div>
+      <div class = "best-value" className = "border best-value col-md-6 text-start ">397</div>
+      </div>
+      <div className ="row align-middle">
+      <div className ="align-items-center col-md-6 text-end  border"><strong>Year Published:</strong></div>
+      <div class = "best-value" className = "border d-flex align-items-center best-value col-md-6 text-start  border">1977</div>
+      </div>
+      <div className ="d-flex row align-middle">
+      <div className ="w-100"></div>
+      <div className ="align-items-center col-md-6 text-end border"><strong>Part of Series:</strong></div>
+      <div class = "best-value" className = "border d-flex align-items-center best-value col-md-6 text-start ">Harry Potter and the Order of the Phoenix</div>
+      </div>
+      <div className ="row align-middle">
+      <div className ="align-items-center col-md-6 text-end border"><strong>Series Name: </strong></div>
+      <div class = "best-value" className = "border d-flex align-items-center best-value col-md-6 text-start ">Harry Potter</div>
+      </div>
+      <div className ="row align-middle">
+      <div className ="align-items-center col-md-6 text-end border"><strong>Place in Series:</strong></div>
+      <div class = "best-value" className = "border d-flex align-items-center best-value col-md-6 text-start ">5</div>
+      </div>
+      <div className ="row align-middle">
+      <div className ="align-items-center col-md-6 text-end border"><strong>Rating:</strong></div>
+      <div class = "best-value" className = "border d-flex align-items-center best-value col-md-6 text-start ">4 stars</div>
+      </div>
+      <div className ="card-body">
+      <p className="card-text">Fantastic book. I was on the edge of my seat the entire time. I can't believe Harry Potter defeated Gandalf to become a Sith Lord.</p>
+      </div>
+      
+      
+      </div>
 )};
 
 export default BookCard;
