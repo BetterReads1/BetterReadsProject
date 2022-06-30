455a3c7d387343166c1181e8520866a803a5d310
Jim White <Jim.White@me.com>
2022-06-30 12:16:06 -0400

    Comments galore!

--- client/components/BookDetails.jsx
+++ client/components/BookDetails.jsx
@@ -13,16 +13,28 @@ import { useDispatch} from 'react-redux'
 
 
 const BookDetails = () => {
-  // class BookDetail extends Component {
-
-  
+/*
+* ==================================================
+*   Set bookGenres to be use react state with
+*   an initial value of empty array.
+!   AND
+*   Allow setBookGenres to be used to update the
+*   state.
+* ==================================================
+*/
   const [bookGenres, setBookGenres] = useState([]);
   // const [bookDetails, setBookDetails] = useState(
   //   {
   //     genres: [],
   //     author: "J"
   //   })
-  
+
+
+/*
+* ==================================================
+*   Temporary data until db connection is complete.
+* ==================================================
+*/
   let props = {};
   props.bookDetail = {
     title: "Carrie",
@@ -34,95 +46,132 @@ const BookDetails = () => {
     pages: 536,
     seriesName: ''
   }
-  
-  const handleIncrement = (product) => {
-    console.log(product);
-    // this.setState({count: this.state.count + 1});
-  };
 
+
+/*
+* ==================================================
+*   useEffect (for React hooks vs. 
+*   ComponentDidMount for React Classes) to actually 
+*   perform the fetch to pull from the server.
+* ==================================================
+*/
   useEffect(() => {
     fetch('http://localhost:3000/genre')
       .then(response => response.json())
       .then(data => { setBookGenres([...data]) });
-
   }, [])
-
+  
+  
+  /*
+  * ==================================================
+  *   Next We want to render multiple copies of an
+  *   HTML element, each with a unique value that was
+  *   returned from our fetch above.
+  * 
+  *   In THIS case, we are generating multiple HTML
+  *   <option> elements to add the retrieved genres
+  *   to a dropdown list.
+  * 
+  *   We do this by initializing an empty array and
+  *   using the Array.map method to push a properly
+  *   structured <option> element for each value 
+  *   returned from our fetch onto the array (which 
+  *   will be rendered below.).
+  * ==================================================
+  */
   const renderGenres = [];
   if (bookGenres) {
     bookGenres.map((genre) => {
-      console.log('🔴🟠🟡🟢🔵🟣 | file: BookDetails.jsx | line 74 | BookDetail | BookDetails | renderGenres', genre);
       renderGenres.push(<option value={genre} key={genre}>{genre}</option>)
-    }
-    )
+    })
   } else {
     renderGenres.push(<p>There are no genres.</p>)
   }
-    console.log('🔴🟠🟡🟢🔵🟣 | file: BookDetails.jsx | line 84 | BookDetail | BookDetails | renderGenres', renderGenres);
 
 
+/*
+* ==================================================
+*   Here we return the HTML elements that make 
+*   up the BookDetails React component.
+* ==================================================
+*
+*   We choose to use Bootstrap creating a responsive
+*   UI to update as sceen dimensions change.
+*
+*   When using Bootstrap with React ... all of the 
+*   bootstrap configuration options are set for each
+*   element in its 'className' property.
+* ==================================================
+*   The display is broken down into a series of 
+*   nested container <div>'s.  This allows for each 
+*   individual <div> to have its own bootstrap 
+*   settings.
+* ==================================================
+*/
   return (
-    <div className='container border border-3 rounded-3 mt-2 p-2 border-info shadow'>
-      <div className="container">
-        <div className='row'>
-        <div className='col-md-12'>
+    <div className='container border border-3 rounded-3 mt-2 p-2 border-info shadow'> //* First container Outter most
+      <div className="container"> //* Second container Inner
+        <div className='row'>     //* 1st row in the container
+        <div className='col-md-12'>   //* Which contains a single column taking the entire width (12 out of 12)
           <label htmlFor='bookTitle'>Title</label>
           <input type='text' className='form-control shadow h-50 border border-primary bg-info bg-opacity-10' id='bookTitle' defaultValue={props.bookDetail.title}></input>
         </div>
       </div>
 
-      <div className='row'>
-        <div className='col-md-6'>
+      <div className='row'>   //* 2nd row in the container
+        <div className='col-md-6'>  //* 1st column in the row using half of the available width (6 out of 12)
           <label htmlFor='inputFirst'>First</label>
           <input type='text' className='form-control shadow h-50 border border-primary bg-info bg-opacity-10' id='inputFirst' defaultValue={props.bookDetail.authorFirst}></input>
         </div>
-        <div className='col-md-6'>
+        <div className='col-md-6'>  //* 2nd column in the row using half of the available width (6 out of 12)
           <label htmlFor='inputLast'>Last</label>
           <input type='text' className='form-control shadow h-50 border border-primary bg-info bg-opacity-10' id='inputLast' defaultValue={props.bookDetail.authorLast}></input>
         </div>
         </div>
         {/* <span className='shadow border border-info px-3 py-1 m-50 btn-light rounded-3 bg-info bg-opacity-10 '>Howdy!</span>
         <p className='border border-3 rounded-3 mt-2 px-2 border-info'>Howdy!</p> */}
-        <div className='row'>
-        <div className='col-md-6'>
+        <div className='row'> //* 3rd row in the container
+        <div className='col-md-6'>  //* 1st of 3 columns in the row using half of the available width (6 out of 12).
         <label htmlFor='selectGenre'>Genre</label>
+        /*
+        * ==================================================
+        *   Here we create our dropdown list of genres whose
+        *   values were retrieved from the db.    
+        * ==================================================
+        */
         <select id='selectGenre' className="col-md-6 form-select shadow h-50 border border-primary bg-info bg-opacity-10" aria-label="Select the Genre">
           <option className="d-flex align-items-start" defaultValue>Select the Genre</option>
+          /*
+          ! ==================================================
+          !   THIS pulls in all the individual <option> 
+          !   elements from the renderGenres variable filled 
+          !   out above (line 82-89)
+          ! ==================================================
+          */
           {renderGenres}
         </select>
       </div>
-
-      {/*    <div className='col-md-6'>
-          <label htmlFor='selectGenre'>Genre</label>
-        <select id='selectGenre' className="col-md-6 form-select shadow h-50 border border-primary bg-info bg-opacity-10" aria-label="Select the Genre">
-            <option className="d-flex align-items-start" selected>Select the Genre</option>
-            <option value="1">Horror</option>
-            <option value="2">Sci-Fi</option>
-            <option value="3">Romance</option>
-        </select>
-         <input type='text' className='form-control shadow h-50 border border-primary bg-info bg-opacity-10' id='inputCity' defaultValue={props.bookDetail.genre}></input>
-        </div> */}
-
-        <div className='col-md-3'>
+        <div className='col-md-3'>  //* 2nd of 3 columns in the row using 3/12 of the width.
           <label htmlFor='inputPhone'>Pages</label>
           <input type='text' className='form-control shadow h-50 border border-primary bg-info bg-opacity-10' id='inputPhone' defaultValue={props.bookDetail.pages}></input>
         </div>
-        <div className='col-md-3'>
+        <div className='col-md-3'>  //* 3rd of 3 columns in the row using 3/12 of the width.
           <label htmlFor='inputEmail'>Year Published</label>
           <input type='text' className='form-control shadow h-50 border border-primary bg-info bg-opacity-10' id='inputEmail' defaultValue={props.bookDetail.yearPublished}></input>
         </div>
       </div>
-      <div className='row'>
-        <div className='col-md-2'>
+      <div className='row'>  //* 4th row in the container
+        <div className='col-md-2'> //* 1st of 2 columns using 2/12 of the width.
           <label htmlFor='partOfSeries'>Part of Series</label>
           <input type='text' className='form-control shadow h-50 border border-primary bg-info bg-opacity-10' id='partOfSeries' defaultValue={props.bookDetail.partOfSeries}></input>
         </div>
-        <div className='col-md-10'>
+        <div className='col-md-10'> //* 2nd of 2 columns using 10/12 of the width.
           <label htmlFor='seriesName'>Series Name</label>
           <input type='text' className='form-control shadow h-50 border border-primary bg-info bg-opacity-10' id='seriesName' defaultValue={props.bookDetail.seriesName}></input>
         </div>
       </div>
-      </div>
-      </div>
+      </div> //* End of Inner Container
+      </div> //* End of Outer Container
   );
 }
 
